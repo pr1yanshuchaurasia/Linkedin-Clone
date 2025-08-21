3fa12891aa4cccf84ed0a965aa1f2ea1818222ee
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-08-21 17:08:53 +0530

    feat(redux): initialize redux store configuration
    Add initial redux store setup using @reduxjs/toolkit to enable state management

---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,10 @@
+// STEPS for State Management
+// Submit Action
+// Handle action in it's reducer
+// Register Here -> Reducer
+
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {}
+})
