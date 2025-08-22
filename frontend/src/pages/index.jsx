2b9e716daf29f3e05e5ec3ced4a8581bfdfab911
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-08-22 17:21:53 +0530

    feat(login): add login page and navigation from home
    Add new login page component and implement navigation from home page.
    The login page includes basic structure and styling module.

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,12 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
 export default function Home() {
+
+  const router = useRouter()
   return (
     <>
     <div className="conatiner">
@@ -15,6 +18,12 @@ export default function Home() {
         <div className="mainContainer__left">
           <p>Connect with Friends without Exaggeration</p>
           <p>A True social media platform, with stories no blufs !</p>
+
+          <div onClick={()=>{
+            router.push("/login")
+          }}className="buttonjoin">
+            <p>Join Now</p>
+          </div>
 
         </div>
          <div className="mainContainer__right">
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoginComponent() {
+  return (
+    <div>
+        LoginComponent
+    </div>
+  )
+}
+
+
+export default LoginComponent;
