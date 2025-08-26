491b01b00eb2be590dadc8fde2fc929e6517ab97
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-08-26 18:13:38 +0530

    feat(navbar): add navbar component to user layout
    Add new Navbar component and integrate it into UserLayout. Includes basic component structure and empty styles module.

---
+++ frontend/src/Components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+
+export default function NavBarComponent () {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
--- frontend/src/layout/UserLayout/index.jsx
+++ frontend/src/layout/UserLayout/index.jsx
@@ -1,8 +1,10 @@
+import NavBarComponent from '@/Components/Navbar'
 import React from 'react'
 
 const UserLayout = ({children}) => {
   return (
     <div>
+      <NavBarComponent/>
       {children}
     </div>
   )
