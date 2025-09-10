d57dd8c7ba579a92b459ea40f735702428ba2704
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-09-10 15:30:15 +0530

    feat(Login): implement login component with layout and styling

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,15 +1,33 @@
-import UserLayout from '@/layout/UserLayout';
-import React from 'react'
+import UserLayout from "@/layout/UserLayout";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
+import styles from "./style.module.css";
 
 function LoginComponent() {
+  const authState = useSelector((state) => state.auth);
+  const router = useRouter();
+
+  const isLoginMethod = useState(false);
+  useEffect(() => {
+    if (authState.loggedIn) {
+      router.push("/dashboard");
+    }
+  });
   return (
     <div>
-        <UserLayout>
-          
-        </UserLayout>
+      <UserLayout>
+        <div className={styles.container}>
+          <div className={styles.cardContainer}>
+            <div className={styles.cardContainer__left}>
+              <p>{isLoginMethod ? "Sign In" : "Sign Up"}</p>
+            </div>
+            <div className={styles.cardContainer__right}></div>
+          </div>
+        </div>
+      </UserLayout>
     </div>
-  )
+  );
 }
-
 
 export default LoginComponent;
