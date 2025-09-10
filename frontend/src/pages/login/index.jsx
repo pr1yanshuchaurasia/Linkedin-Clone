57a3d27d7e9a4d24bf4d8e6d2efd987a98d64f38
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-09-10 15:50:01 +0530

    feat(Login): update login component with user login method and input fields

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -8,7 +8,7 @@ function LoginComponent() {
   const authState = useSelector((state) => state.auth);
   const router = useRouter();
 
-  const isLoginMethod = useState(false);
+  const [userLoginMethod, setUserLoginMethod] = useState(false);
   useEffect(() => {
     if (authState.loggedIn) {
       router.push("/dashboard");
@@ -20,7 +20,24 @@ function LoginComponent() {
         <div className={styles.container}>
           <div className={styles.cardContainer}>
             <div className={styles.cardContainer__left}>
-              <p>{isLoginMethod ? "Sign In" : "Sign Up"}</p>
+              <p className={styles.cardleft__heading}>
+                {userLoginMethod ? "Sign In" : "Sign Up"}
+              </p>
+              <div className={styles.inputContainers}>
+                <div className={styles.inputRow}>
+                  {" "}
+                  <input
+                    className={styles.inputField}
+                    type="text"
+                    placeholder="Username"
+                  />{" "}
+                  <input
+                    className={styles.inputField}
+                    type="text"
+                    placeholder="Name"
+                  />
+                </div>
+              </div>
             </div>
             <div className={styles.cardContainer__right}></div>
           </div>
