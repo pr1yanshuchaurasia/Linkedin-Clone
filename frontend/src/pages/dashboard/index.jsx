3a6285b5dc71d4de319690b72116eff71f656211
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-09-30 12:52:43 +0530

    feat(Dashboard): add token validation state management in dashboard component

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 export default function Dashboard() {
 
-const router = useRouter();
+  const router = useRouter();
+
+  const [isTokenThere, setIsTokenThere] = React.useState(false);
+
     useEffect(()=>{
         if (localStorage.getItem("token") === null){
             router.push("/login")
         }
+        setIsTokenThere(true);
 
     },[router])
+
+    useEffect(()=>{
+
+    },[isTokenThere])
   return (
     <div>
       Dashboard
