a842b89600c62b3efb9deb0bb4bbdc39cef22cc9
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-10-07 15:24:01 +0530

    fix(Dashboard): correct dispatch function name and ensure posts are fetched when token is present

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { useDispatch } from 'react-redux';
+import { getAllPosts } from '@/config/redux/action/postAction';
 
 export default function Dashboard() {
 
   const router = useRouter();
+  const dispath = useDispatch();
 
   const [isTokenThere, setIsTokenThere] = React.useState(false);
 
@@ -16,6 +19,9 @@ export default function Dashboard() {
     },[router])
 
     useEffect(()=>{
+      if(isTokenThere){
+        dispath(getAllPosts())
+      }
 
     },[isTokenThere])
   return (
