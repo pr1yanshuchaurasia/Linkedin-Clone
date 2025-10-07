43b8866db138d3da0b0909ed232c2293a18de79f
Priyanshu Chaurasia <priyanshuchaurasia8840@gmail.com>
2025-10-07 14:25:35 +0530

    feat(PostReducer): implement post slice with initial state and async actions for fetching posts

---
+++ frontend/src/config/redux/reducer/postReducer/index.js
@@ -0,0 +1,43 @@
+const { createSlice } = require("@reduxjs/toolkit");
+const { init } = require("next/dist/compiled/webpack/webpack");
+
+const initialState = {
+  posts: [],
+  isError: false,
+  postFetched: false,
+  isLoading: false,
+  loggedIn: false,
+  message: "",
+  comments: [],
+  postID: "",
+};
+
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    reset: () => initialState,
+    resetPostId: (state) => {
+      state.postID = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllPosts.pending, (state) => {
+        state.isLoading = true;
+        state.message = "Fetching all the Posts....";
+      })
+      .addCase(getAllPosts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.postFetched = true;
+        state.posts = action.payload.posts;
+        state.message = "All Posts Fetched Successfully";
+      })
+      .addCase(getAllPosts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+  },
+});
